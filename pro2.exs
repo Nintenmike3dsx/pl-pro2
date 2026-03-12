249b6fb67e77f479fd1838e673f5e94035512779
Michael Arend <marend001@student.franciscan.edu>
2026-03-12 18:34:03 -0400

    First Step: Splitting Sentances

--- pro2.exs
+++ pro2.exs
@@ -1,1 +1,8 @@
-IO.puts("Hello, World!")
+defmodule Project2 do
+
+  # Splits sentences at every . ! or ? and removes whitespace
+  def sentences(text),
+    do: text |> String.split(~r/[.!?]+/) |> Enum.map(&String.trim/1) |> Enum.reject(&(&1 == ""))
+
+end
+IO.inspect Project2.sentences("Test Sentance. Hello world. Does this work? I am a cool guy!    ")
