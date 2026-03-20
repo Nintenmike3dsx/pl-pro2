0e9d7d742e29efb07824f71e16e216defa1ecb0d
Nintenmike3dsx <mjarend04@gmail.com>
2026-03-19 20:11:20 -0400

    Added check for if the user enters nothing, fixing arithmetic error

--- pro2.exs
+++ pro2.exs
@@ -13,20 +13,26 @@ defmodule Project2 do
   def freq([h | t]), do: freq(t) |> Map.update(h, 1, fn n -> n + 1 end)
 
   # Extra - Average Word Length
+  def avg([]), do: 0.0
   def avg(words),
     do: words |> Enum.map(&String.length/1) |> Enum.reduce(0, fn n, acc -> acc + n end) |> then(fn total -> total / length(words) end)
 
   def run(text) do
     words = words(text) # List of Words
-    freq  = freq(words) # Count How Often Each Word Appears
-    top3  = freq |> Enum.sort_by(fn {_, n} -> n end, :desc) |> Enum.take(3) # sort by count and take top 3 words
 
-    IO.puts("Words: #{length(words)}") # Total Word Count
-    IO.puts("Sentences: #{length(sentences(text))}") # Total Sentence Count
-    IO.puts("Unique: #{map_size(freq)}") # Number of Different Words
-    IO.puts("\nTop 3 words:") # Top 3 Words
-    Enum.each(top3, fn {word, n} -> IO.puts("  #{word}: #{n}") end) # Display Each Top Word and How Many Times it Came Up
-    IO.puts("Average Word Length: #{Float.round(avg(words), 1)} characters") # Average Word Length
+    if words == [] do
+      IO.puts("You didnt enter anything :(")
+    else
+      freq  = freq(words) # Count How Often Each Word Appears
+      top3  = freq |> Enum.sort_by(fn {_, n} -> n end, :desc) |> Enum.take(3) # sort by count and take top 3 words
+
+      IO.puts("Words: #{length(words)}") # Total Word Count
+      IO.puts("Sentences: #{length(sentences(text))}") # Total Sentence Count
+      IO.puts("Unique: #{map_size(freq)}") # Number of Different Words
+      IO.puts("\nTop 3 words:") # Top 3 Words
+      Enum.each(top3, fn {word, n} -> IO.puts("  #{word}: #{n}") end) # Display Each Top Word and How Many Times it Came Up
+      IO.puts("Average Word Length: #{Float.round(avg(words), 1)} characters") # Average Word Length
+    end
   end
 end
 IO.puts("Enter your sentence:") # Prompt user for sentence
